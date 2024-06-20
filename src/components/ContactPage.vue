a3c0d343f1cf2be085e8ff529e16df2d492bc35f
Blake <blake-hammond@live.com>
2024-06-20 10:42:36 +0930

    Instagram tiles on contact page, hyperlinks added

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -10,25 +10,22 @@
             To get in touch with Chelsea, please email <a href="mailto:chelsea@tatarahealth.com.au?subject=Website%20Inquiry">chelsea@tatarahealth.com.au</a><br>
             or call 0466 108 445 <br>
             Instagram: <a href="https://www.instagram.com/tatara.health/">@tatara.health</a><br>
-            Facebook: 
+            Facebook: <a href="https://www.facebook.com/profile.php?id=61557057877849">Tatara Health</a>
           </p>
           <br>
 
           <div class="content-wrapper-horizontal">
-          <!--Placeholders
-            <a href=”https://www.google.com“> <img src=”your_image.gif” width=”100″ height=”132″> </a>
-            <a href=”https://www.google.com“> <img src=”your_image.gif” width=”100″ height=”132″> </a>
-            <a href=”https://www.google.com“> <img src=”your_image.gif” width=”100″ height=”132″> </a>
-            <a href=”https://www.google.com“> <img src=”your_image.gif” width=”100″ height=”132″> </a>
-            <a href=”https://www.google.com“> <img src=”your_image.gif” width=”100″ height=”132″> </a>
-            <a href=”https://www.google.com“> <img src=”your_image.gif” width=”100″ height=”132″> </a>
-          -->
+          
+            <a class="link-container" href="https://www.instagram.com/p/C6svxLlywcQ/?img_index=1"> <img class="linked-image" src="D:\Adelaide Uni Diploma\Projects\tatara-health\tatara-health\src\assets\96DB1478-F75F-41B8-BAF4-E869622EF73F 2.jpeg">  </a>
+            <a class="link-container" href="https://www.instagram.com/p/C8TxwAASTX7/?img_index=1"> <img class="linked-image" src="D:\Adelaide Uni Diploma\Projects\tatara-health\tatara-health\src\assets\COFFEE 2.jpeg" > </a>
+            <a class="link-container" href="https://www.instagram.com/p/C7LuXu0yoPI/?img_index=1"> <img class="linked-image" src="D:\Adelaide Uni Diploma\Projects\tatara-health\tatara-health\src\assets\DANDELION 2.jpeg" > </a>
+            <a class="link-container" href="https://www.instagram.com/p/C68Ib1Uys5d/?img_index=1"> <img class="linked-image" src="D:\Adelaide Uni Diploma\Projects\tatara-health\tatara-health\src\assets\MORNING SUNLIGHT EXPOSURE 2.jpeg" > </a>
+            <a class="link-container" href="https://www.instagram.com/p/C7Vq6RMyMGF/?img_index=1"> <img class="linked-image" src="D:\Adelaide Uni Diploma\Projects\tatara-health\tatara-health\src\assets\6887433F-82A7-4E9B-B826-2D2847D4CD81 2.jpeg" > </a>
+          
           </div>
           
           
-          <div id="image-centered">
-            <img src="D:\Adelaide Uni Diploma\Projects\tatara-health\tatara-health\src\assets\IMG_0942.png" alt="Chelsea standing in a paddock next to an old gum tree" id="main-image">
-          </div>
+          
 
         </div>
 
@@ -45,6 +42,20 @@
   <style scoped>
   /* Add your styles for the homepage here */
   
+  a:link, a:visited, a:hover, a:active {
+    color: black;
+  }
+
+  .linked-image {
+    height: 100%;
+    width: 100%;
+  }
+
+  .link-container {
+    align-items: center;
+    padding: 20px;
+  }
+
   main {
     padding: 20px;
     padding-left: 15%;
